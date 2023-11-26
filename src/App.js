803e88fdbaa73ba56e378b2cbbea3703c34878c7
Rossita <47862499+AaronsonRossita@users.noreply.github.com>
2023-11-26 20:57:47 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  let name1 = "Tom"
+  let name2 = "Marry"
+  let name3 = "Jack"
+
+  return(
+    <div>
+      <h1>{name1}</h1>
+      <h2>{name2}</h2>
+      <h3>{name3}</h3>
     </div>
-  );
+  )
 }
 
 export default App;
